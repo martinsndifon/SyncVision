6cb23251009f42d006dbfd0371983faab5617535
martinsndifon <martinsndifon@gmail.com>
2023-09-11 10:50:37 +0100

    Join users to room using socketio

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 """main website flask app"""
 from flask import Flask
 from views import app_views
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, send
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -14,5 +15,22 @@ app.register_blueprint(app_views)
 def handle_my_custom_event(data):
     print('received json: ', data)
 
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
--- static/scripts/call.js
+++ static/scripts/call.js
@@ -1,8 +1,18 @@
-'use strict'
+'use strict';
 
 const socket = io();
-socket.on('connect', function() {
-  socket.emit('my event', {data: 'I\'m connected!'});
+socket.on('connect', function () {
+  socket.emit('my event', { data: "I'm connected!" });
+  socket.emit('join', { username: 'Martins', room: roomId });
 });
 
-console.log('RoomId', roomId);
+socket.on('message', (message) => {
+  displayMessage(message);
+});
+
+function displayMessage(message) {
+  const messagesDiv = document.getElementById('messages');
+  const messageElement = document.createElement('div');
+  messageElement.textContent = message;
+  messagesDiv.appendChild(messageElement);
+}
